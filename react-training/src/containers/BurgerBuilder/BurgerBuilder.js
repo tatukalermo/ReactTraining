c0de0fa556c539137cd8fa45b086606021b16287
tkalermo <tatu.kalermo@eficode.com>
2021-01-14 17:56:45 +0200

    Adds Order now button

--- react-training/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-training/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,21 +4,33 @@ import Burger from "../../components/Burger/Burger";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
+  bacon: 0.8,
   cheese: 0.4,
   meat: 1.3,
-  bacon: 0.7,
 };
 
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 2,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     },
-    totalPrice: 4,
+    totalPrice: 0,
+    purchaseable: false,
   };
+
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchaseable: sum > 0 });
+  }
 
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
@@ -31,6 +43,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   removeIngredientHandler = (type) => {
@@ -47,6 +60,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   render() {
@@ -63,6 +77,8 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
+          price={this.state.totalPrice}
         />
       </React.Fragment>
     );
