03f2086452e10f47520458303d96525a238d9e8a
tkalermo <tatu.kalermo@eficode.com>
2021-01-15 17:16:14 +0200

    Adds backdrop for the modal to cancel purchase

--- react-training/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-training/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,6 +70,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -79,7 +83,10 @@ class BurgerBuilder extends Component {
     }
     return (
       <React.Fragment>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
