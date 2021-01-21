d9c40a6e1a1f9c2a5027e2238155a29955005329
tkalermo <tatu.kalermo@eficode.com>
2021-01-21 18:24:21 +0200

    Adds sidedrawer

--- react-training/src/components/Layout/Layout.js
+++ react-training/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => {
   return (
     <React.Fragment>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
   );
